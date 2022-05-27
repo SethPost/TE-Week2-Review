de9c4fd7a8897d592d5e5dcc5669fb3bb9c19903
Seth Post <postsa@miamioh.edu>
2022-05-27 18:04:00 -0400

    Updated Orange Week 2 Team 6 Pairs Exercise

--- src/main/java/com/techelevator/Application.java
+++ src/main/java/com/techelevator/Application.java
@@ -1,6 +1,16 @@
 package com.techelevator;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
+
+    List<Department> departments = new ArrayList<Department>();
+    List<Employee> employees = new ArrayList<Employee>();
+    Map<String, Project> projects = new HashMap<>();
 
     /**
      * The main entry point in the application
@@ -17,6 +27,7 @@ public class Application {
 
         // print each department by name
         printDepartments();
+
 
         // create employees
         createEmployees();
@@ -39,6 +50,13 @@ public class Application {
      * Create departments and add them to the collection of departments
      */
     private void createDepartments() {
+        com.techelevator.Department marketing = new com.techelevator.Department(1, "Marketing");
+        com.techelevator.Department sales = new com.techelevator.Department(2, "Sales");
+        com.techelevator.Department engineering = new com.techelevator.Department(3, "Engineering");
+        departments.add(marketing);
+        departments.add(sales);
+        departments.add(engineering);
+
     }
 
     /**
@@ -46,6 +64,9 @@ public class Application {
      */
     private void printDepartments() {
         System.out.println("------------- DEPARTMENTS ------------------------------");
+        for (com.techelevator.Department department : departments) {
+            System.out.println(department.getName());
+        }
 
     }
 
@@ -53,6 +74,21 @@ public class Application {
      * Create employees and add them to the collection of employees
      */
     private void createEmployees() {
+        Employee deanJohnson = new Employee();
+            deanJohnson.setEmployeeID(1);
+            deanJohnson.setFirstName("Dean");
+            deanJohnson.setLastname("Johnson");
+            deanJohnson.setEmail("djohnson@teams.com");
+            deanJohnson.setDepartment(departments.get(2));
+            deanJohnson.setHireDate("08/21/2020");
+        Employee angieSmith = new Employee(2, "Angie",
+                "Smith", "asmith@teams.com", departments.get(2), "08/21/2020");
+        Employee margaretThompson = new Employee(3, "Margaret",
+                "Thompson", "mthompson@teams.com", departments.get(0), "08/21/2020");
+        employees.add(deanJohnson);
+        employees.add(angieSmith);
+        employees.add(margaretThompson);
+        angieSmith.raiseSalary(0.10);
 
     }
 
@@ -61,6 +97,10 @@ public class Application {
      */
     private void printEmployees() {
         System.out.println("\n------------- EMPLOYEES ------------------------------");
+        for (Employee employee : employees) {
+            System.out.println(employee.getFullName() + " (" + employee.getSalary() + ") "
+                    + employee.getDepartment().getName());
+        }
 
     }
 
@@ -68,14 +108,32 @@ public class Application {
      * Create the 'TEams' project.
      */
     private void createTeamsProject() {
-
+        Project teams = new Project("TEams",
+                "Project Management Software", "10/10/2020", "11/10/2020");
+        List <Employee> engineeringEmployees = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee.getDepartment().getName().equals("Engineering")) {
+                engineeringEmployees.add(employee);
+            }
+        }
+        teams.setTeamMembers(engineeringEmployees);
+        projects.put("Management Software", teams);
     }
 
     /**
      * Create the 'Marketing Landing Page' project.
      */
     private void createLandingPageProject() {
-
+        Project marketingLandingPage = new Project ("Marketing Landing Page",
+                "Lead Capture Landing Page for Marketing", "10/10/2020", "10/17/2020");
+        List <Employee> marketingEmployees = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee.getDepartment().getName().equals("Marketing")) {
+                marketingEmployees.add(employee);
+            }
+        }
+        marketingLandingPage.setTeamMembers(marketingEmployees);
+        projects.put("Marketing Landing Page", marketingLandingPage);
     }
 
     /**
@@ -83,7 +141,9 @@ public class Application {
      */
     private void printProjectsReport() {
         System.out.println("\n------------- PROJECTS ------------------------------");
-
+        System.out.println(projects.get("Management Software").getName() + ": " +
+                projects.get("Management Software").getTeamMembers().size());
+        System.out.println(projects.get("Marketing Landing Page").getName() + ": " +
+                projects.get("Marketing Landing Page").getTeamMembers().size());
     }
-
 }
